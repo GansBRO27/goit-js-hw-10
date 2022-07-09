7a67d0f60c732ba5770c39290b48f2b9e4111ead
GansBRO27 <vladyslavgdjmg@gmail.com>
2022-07-09 22:42:06 +0300

    hw

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+let debounce = require('lodash.debounce');
+
+const DEBOUNCE_DELAY = 300;
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages `
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+const inputEl = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+function createMarkup(countrie) {
+  const mark = countrie
+    .map(({ capital, flags, languages, name, population }) => {
+      const lang = Object.values(languages);
+      return `<li class='countrie-list__item'><img src='${flags.svg}' class='countrie__img'><sapan class='countrie__text'>${name.official}</span></li>
+      <li class='countrie__item'><sapan class='countrie__text'>Capital:${capital}</span></li>
+      <li class='countrie__item'><sapan class='countrie__text'>Population:${population}</span></li>
+      <li class='countrie__item'><sapan class='countrie__text'>Languages:${lang}</span></li>`;
+    })
+    .join('');
+  countryList.innerHTML = mark;
+}
+
+function createMarkupList(countrie) {
+  const mark = countrie
+    .map(({ capital, flags, languages, name, population }) => {
+      const lang = Object.values(languages);
+      console.log(capital[0], flags.svg, lang, name.official, population);
+      return `<li class='countrie-list__item'><img src='${flags.svg}' class='countrie-list__img'><sapan class='class-list__text'>${name.official}</span></li>`;
+    })
+    .join('');
+  countryList.innerHTML = mark;
+  console.log(mark);
+}
+
+inputEl.addEventListener(
+  'input',
+  debounce(e => {
+    e.preventDefault();
+    countryList.innerHTML = '';
+    fetchCountries(e.target.value.trim())
+      .then(countrie => {
+        if (countrie.length > 1 && countrie.length <= 10) {
+          createMarkupList(countrie);
+        }
+        if (countrie.length === 1) {
+          createMarkup(countrie);
+        }
+        if (countrie.length > 10) {
+          Notify.info('please more specific name');
+        }
+      })
+
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+      });
+  }, 300)
+);
